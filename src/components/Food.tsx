668a228894e5797d09b714ca7db4f4ffa8f1e107
xkccoding <xkccoding@gmail.com>
2022-10-09 21:51:09 +0800

    ✨ feat: Food组件封装

---
+++ src/components/Food.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Food = () => {
+	return (
+		<div>
+			<div>
+				<img src="https://images.unsplash.com/photo-1607013251379-e6eecfffe234?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnVyZ2Vyc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60" alt="" />
+			</div>
+			<div>
+				<p></p>
+				<span className=' rounded-full text-white bg-orange-300'>$$$</span>
+			</div>
+		</div>
+	)
+}
+
+export default Food
---
+++ src/components/PopularFood.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const PopularFood = () => {
+	const filterTypes = ['All', 'Burgers', 'Pizza', 'Salad', 'chicken']
+	const filterPrices = ['$', '$$', '$$$', '$$$$']
+	return (
+		<div className='max-w-7xl mx-auto'>
+			<h1 className=' text-center text-3xl text-orange-500 font-bold'>Top Rated Menu Items</h1>
+			<div>
+				<h4>Filter Type</h4>
+				<div className=' flex justify-between'>
+					{
+						filterTypes.map(item => <button className='text-orange-500 border-orange-500'>{item}</button>)
+					}
+				</div>
+				<h4>Filter Price</h4>
+				<div className=' flex gap-8'>
+					{
+						filterPrices.map(item => <button className='text-orange-500 border-orange-500'>{item}</button>)
+					}
+				</div>
+
+			</div>
+		</div>
+	)
+}
+
+export default PopularFood
