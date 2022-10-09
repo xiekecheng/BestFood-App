1cd2f594ca834d5d503c9e3ec400cb5986a80831
xkccoding <xkccoding@gmail.com>
2022-10-09 21:53:07 +0800

    ✨ feat: 引入组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import HeadlineCards from "./components/HeadlineCards"
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
+import PopularFood from "./components/PopularFood"
 
 function App() {
   return (
-    <div>
+    <div className="px-2">
       <Navbar />
+      <Hero />
+      <HeadlineCards />
+      <PopularFood />
     </div>
   )
 }
